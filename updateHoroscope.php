e2b8a2ca471d3e271e4fda5b9909a07a3314a547
andremedie <32762205+andremedie@users.noreply.github.com>
2018-04-04 18:58:58 +0200

    Add files via upload

--- updateHoroscope.php
+++ updateHoroscope.php
@@ -4,10 +4,10 @@ parse_str(file_get_contents("php://input"), $_PUT);
 include('class.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+    $_POST['personnummer'] = $_PUT['personnummer'];
     if (isset($_SESSION['personnummer'])) {
-        $_POST['personnummer'] = $_PUT['personnummer'];
-         $_SESSION['personnummer'] = $Andre->horoskop;
-     echo "true";
+        $_SESSION['personnummer'] = $Andre->horoskop;
+        echo "true";
      
         }else{
          echo "false1";
@@ -15,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
     
     
 } else {
-    echo "false2";
+    echo "false";
 }
 
 
